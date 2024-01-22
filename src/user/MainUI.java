b99a65b5f79072d24a337e0014563e44fc3b5a53
Savindu <savinduruhunuhewa@gmail.com>
2024-01-22 07:29:00 +0530

    create the MainUI

---
+++ src/user/MainUI.java
@@ -0,0 +1,73 @@
+package user;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+import java.awt.event.KeyEvent;
+import java.awt.event.InputEvent;
+
+public class MainUI extends JFrame {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainUI frame = new MainUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainUI() {
+		setTitle("Electricity Billing Dashboard");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 866, 604);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnHome = new JMenu("Home");
+		menuBar.add(mnHome);
+		
+		JMenuItem mntmHome = new JMenuItem("Home");
+		mntmHome.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_H, InputEvent.CTRL_DOWN_MASK));
+		mnHome.add(mntmHome);
+		
+		JMenu mnPayment = new JMenu("Payment");
+		menuBar.add(mnPayment);
+		
+		JMenuItem mntmBillPayment = new JMenuItem("Bill Payment");
+		mntmBillPayment.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, InputEvent.CTRL_DOWN_MASK));
+		mnPayment.add(mntmBillPayment);
+		
+		JMenu mnLogout = new JMenu("Logout");
+		menuBar.add(mnLogout);
+		
+		JMenuItem mntmLogOut = new JMenuItem("LogOut");
+		mntmLogOut.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_L, InputEvent.CTRL_DOWN_MASK));
+		mnLogout.add(mntmLogOut);
+		
+		JMenu mnProfile = new JMenu("Profile");
+		menuBar.add(mnProfile);
+		
+		JMenuItem mntmMyProfile = new JMenuItem("My Profile");
+		mntmMyProfile.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, InputEvent.CTRL_DOWN_MASK | InputEvent.ALT_DOWN_MASK));
+		mnProfile.add(mntmMyProfile);
+	}
+}
