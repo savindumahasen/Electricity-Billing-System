7af23e05aab4b989c212662c8eb89d32bdd9984e
Savindu <savinduruhunuhewa@gmail.com>
2024-01-24 15:19:37 +0530

    Update the MyProfile UI

---
+++ src/buisness/MyProfile.java
@@ -0,0 +1,22 @@
+package buisness;
+
+public class MyProfile {
+	
+	private String firstName;
+	private String lastName;
+
+	public MyProfile() {
+		// TODO Auto-generated constructor stub
+		this.firstName=firstName;
+		this.lastName=lastName;
+		
+		
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+
+}
