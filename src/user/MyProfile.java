a84a3ad6f998165b8c564ed88564c6a85934913b
Savindu <savinduruhunuhewa@gmail.com>
2024-01-24 13:58:54 +0530

    Create the Home and MyProfile UI

---
+++ src/user/MyProfile.java
@@ -0,0 +1,101 @@
+package user;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import buisness.Customer;
+import data.CustomerDB;
+import buisness.Customer;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+
+public class MyProfile extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtFirstName;
+	private CustomerDB customer;
+	private JTextField txtLastName;
+    private CustomerDB custDB;
+    private JButton btnUpdatePassword;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MyProfile frame = new MyProfile();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MyProfile() {
+		setTitle("My Profile");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 654, 497);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		custDB=new CustomerDB();
+		
+		customer=new CustomerDB();
+		
+		txtFirstName = new JTextField();
+		txtFirstName.setBounds(181, 77, 280, 28);
+		contentPane.add(txtFirstName);
+		txtFirstName.setColumns(10);
+		ArrayList<Customer> custList=custDB.getAll();
+		for(Customer customerList:custList) {
+		txtFirstName.setText(customerList.getFirstName());
+		}
+	
+		
+		txtLastName = new JTextField();
+		txtLastName.setBounds(181, 142, 274, 28);
+		contentPane.add(txtLastName);
+		txtLastName.setColumns(10);
+		ArrayList<Customer> custList1=custDB.getAll();
+		for(Customer customerList2:custList1) {
+		txtLastName.setText(customerList2.getLastName());
+		}
+		
+		JButton btnUpdate = new JButton("Edit Profile");
+		btnUpdate.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnUpdate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String firstName=txtFirstName.getText();
+				String lastName=txtLastName.getText();
+			
+			}
+		});
+		btnUpdate.setBounds(76, 279, 510, 21);
+		contentPane.add(btnUpdate);
+		
+		btnUpdatePassword = new JButton("Change Password");
+		btnUpdatePassword.setFont(new Font("Tahoma", Font.BOLD, 14));
+		btnUpdatePassword.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnUpdatePassword.setBounds(76, 322, 510, 21);
+		contentPane.add(btnUpdatePassword);
+		setLocationRelativeTo(this);
+	}
+}
