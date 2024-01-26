a65aa547327c227c72078364e9870995706a4e48
Savindu <savinduruhunuhewa@gmail.com>
2024-01-26 19:28:08 +0530

    Update the BillPaymentUI

--- src/user/BillPaymentUI.java
+++ src/user/BillPaymentUI.java
@@ -20,6 +20,7 @@ import buisness.Customer;
 import data.CustomerDB;
 import data.PaymentDB;
 import buisness.Payment;
+import java.awt.Color;
 
 public class BillPaymentUI extends JFrame {
 
@@ -30,6 +31,9 @@ public class BillPaymentUI extends JFrame {
 	private PaymentDB paymentDB;
 	private JTextField txtAccount;
 	private JButton btnBack;
+	private JTextField txtID;
+	private JLabel lblID;
+	private JTextField txtID1;
 
 	/**
 	 * Launch the application.
@@ -53,72 +57,86 @@ public class BillPaymentUI extends JFrame {
 	public BillPaymentUI() {
 		setTitle("Online Payment");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 723, 551);
+		setBounds(100, 100, 723, 478);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.ORANGE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		setLocationRelativeTo(this);
+		
+
+		
 		JLabel lblAccount = new JLabel("Account to be paid");
 		lblAccount.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblAccount.setToolTipText("");
-		lblAccount.setBounds(70, 112, 159, 17);
+		lblAccount.setBounds(70, 144, 159, 17);
 		contentPane.add(lblAccount);
 		
 		txtAccount = new JTextField();
-		txtAccount.setBounds(428, 113, 198, 19);
+		txtAccount.setBounds(428, 145, 198, 19);
 		contentPane.add(txtAccount);
 		txtAccount.setColumns(10);
 		
 		customerDB=new CustomerDB();
 		paymentDB=new PaymentDB();
-		ArrayList<Customer> cust1=customerDB.getAll();
-		for(Customer custList1:cust1) {
-			txtAccount.setText(custList1.getAccountNumber()+" "+custList1.getFirstName()+" "+custList1.getLastName());
-		}
 		
+		lblID = new JLabel("ID\r\n");
+		lblID.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblID.setBounds(70, 94, 78, 13);
+		contentPane.add(lblID);
+		
+		txtID1 = new JTextField();
+		txtID1.setColumns(10);
+		txtID1.setBounds(428, 91, 198, 19);
+		contentPane.add(txtID1);
+	
 		JLabel lblAmount = new JLabel("Amount");
 		lblAmount.setToolTipText("");
 		lblAmount.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblAmount.setBounds(70, 202, 159, 17);
+		lblAmount.setBounds(70, 205, 159, 17);
 		contentPane.add(lblAmount);
 		
 		txtAmount = new JTextField();
-		txtAmount.setBounds(425, 203, 201, 19);
+		txtAmount.setBounds(428, 206, 201, 19);
 		contentPane.add(txtAmount);
 		txtAmount.setColumns(10);
 		
 		JLabel lblEmail = new JLabel("Email Address");
 		lblEmail.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblEmail.setBounds(70, 309, 132, 13);
+		lblEmail.setBounds(70, 272, 132, 13);
 		contentPane.add(lblEmail);
 		
 		txtEmail = new JTextField();
-		txtEmail.setBounds(425, 306, 201, 19);
+		txtEmail.setBounds(428, 271, 201, 19);
 		contentPane.add(txtEmail);
 		txtEmail.setColumns(10);
 		
 		JButton btnProceed = new JButton("Proceed");
 		btnProceed.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String account="";
-				ArrayList<Customer> custList=customerDB.getAll();
-				   account=txtAccount.getText();
+				   int ID=Integer.valueOf(txtID1.getText());
+				   String account=txtAccount.getText();
 				   double amount=Double.valueOf(txtAmount.getText());
 				   String email=txtEmail.getText();
-				   Payment payment=new Payment(account,amount,email);
-				   int result=paymentDB.add(payment);
-				   if(result==1) {
+				   Customer customer=customerDB.get(ID);
+				   if(account.equals(customer.getAccountNumber())){
+                     Payment payment=new Payment(account,amount,email);
+				     int result=paymentDB.add(payment);
+				     if(result==1) {
 					   JOptionPane.showMessageDialog(null,"Payment transaction is sucessfully  completed");
-				   }else {
+				     }else {
 					   JOptionPane.showMessageDialog(null,"Payment transaction is not sucessfully  completed");
+				     }
+				
+				   }else {
+					JOptionPane.showMessageDialog(null,"Please Enter the correct Account Number");
 				   }
-				
-				
 			}
 		});
 		btnProceed.setFont(new Font("Tahoma", Font.BOLD, 14));
-		btnProceed.setBounds(362, 424, 108, 21);
+		btnProceed.setBounds(367, 369, 108, 21);
 		contentPane.add(btnProceed);
 		
 		btnBack = new JButton("Back\r\n");
@@ -130,8 +148,11 @@ public class BillPaymentUI extends JFrame {
 			}
 		});
 		btnBack.setFont(new Font("Tahoma", Font.BOLD, 14));
-		btnBack.setBounds(163, 424, 108, 21);
+		btnBack.setBounds(150, 369, 108, 21);
 		contentPane.add(btnBack);
+		
+	
+	
 		
 	
 	}
